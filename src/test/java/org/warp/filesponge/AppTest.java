ef9333e7276aec182e9040ac0d1ee06729ed1e45
Andrea Cavalli <andrea@cavallium.it>
2020-09-23 22:45:55 +0200

    First commit

---
+++ src/test/java/org/warp/filesponge/AppTest.java
@@ -0,0 +1,19 @@
+package org.warp.filesponge;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for FileSponge.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
